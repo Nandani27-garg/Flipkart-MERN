e3990338bbcdeaa9a5b37ca3c5daadc45b60f861
Nandani Garg <gargnandani2004@gmail.com>
2026-09-03 09:47:48 +0530

    asdf

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,21 @@
 const mongoose = require('mongoose');
-const MONGO_URI = process.env.MONGO_URI;
 
-const connectDatabase = () => {
-    mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => {
-            console.log("Mongoose Connected");
-        });
-}
+mongoose.set('strictQuery', false);
+
+const connectDatabase = async () => {
+    try {
+        const MONGO_URI = process.env.MONGO_URI;
+
+        if (!MONGO_URI) {
+            throw new Error('MONGO_URI is not defined in environment variables');
+        }
+
+        await mongoose.connect(MONGO_URI);
+        console.log('Mongoose Connected');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+        process.exit(1);
+    }
+};
 
 module.exports = connectDatabase;
